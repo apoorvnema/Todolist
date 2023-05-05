819ed07b1616f731074a32ce75237917e7acd198
apoorvnema <68653039+apoorvnema@users.noreply.github.com>
2023-05-05 11:55:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Storing Password and API keys
-const dbPassword = process.env.password;
 const url = process.env.url;
 
 mongoose.connect(url);
